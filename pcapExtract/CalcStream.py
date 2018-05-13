6e8573103dfbadb25014227726e90d736f26834c
Leo Yan <phyan@internal.synopsys.com>
2018-05-13 22:30:51 +0800

    extract pcap finished

--- pcapExtract/CalcStream.py
+++ pcapExtract/CalcStream.py
@@ -12,24 +12,107 @@ CWR = 0x80
 
 pcaps = rdpcap("./input2.pcap")
 
-src_dst = {}
+pkt = {}
+non_tcp = 0
 syn = 0
 fin = 0
+long_stream = 0
+short_stream = 0
+long_stream_count = 0
+short_stream_count = 0
+long_pcaps = []
+short_pcaps = []
 
+i = -1
 for packet in pcaps:
-    src = packet.src
-    dst = packet.dst
-    # print(src, dst)
-    if src not in src_dst.keys():
-        src_dst[src] = defaultdict(list)
+    if packet['IP'].proto != 6:
+        non_tcp += 1
+        continue
+    src_ip = packet['IP'].src
+    dst_ip = packet['IP'].dst
+    ip_tuple = (src_ip, dst_ip)
+    ip_tuple1 = (dst_ip, src_ip)
+    src_port = packet['TCP'].sport
+    dst_port = packet['TCP'].dport
+    port_tuple = (src_port, dst_port)
+    port_tuple1 = (dst_port, src_port)
+    # print(ip_tuple, port_tuple)
+    if ip_tuple not in pkt.keys():
+        pkt[ip_tuple] = {}
+        pkt[ip_tuple1] = {}
+
+    if port_tuple1 not in pkt[ip_tuple1].keys():
+        pkt[ip_tuple1][port_tuple1] = defaultdict(list)
+    if port_tuple not in pkt[ip_tuple].keys():
+        pkt[ip_tuple][port_tuple] = defaultdict(list)
+
+    # print("begin", pkt[ip_tuple][port_tuple])
     f = packet['TCP'].flags
     if f & FIN:
-        if src == "00:05:5d:21:99:4c" and dst == "00:21:6a:5b:7d:4a":
-            fin += 1
-        src_dst[src][dst].append(('fin', packet.time))
-    if f & SYN:
-        if src == "00:05:5d:21:99:4c" and dst == "00:21:6a:5b:7d:4a":
-            syn += 1
-        src_dst[src][dst].append(('syn', packet.time))
-print(syn, fin)
-print(src_dst["00:05:5d:21:99:4c"]["00:21:6a:5b:7d:4a"])
+
+        # if delta> 1000:
+        #     long_stream_count += 1
+        #     long_stream += delta
+        #     long_pcaps.append(syn_pkt)
+        #     long_pcaps.append(packet)
+        # else:
+        #     long_pcaps.append(syn_pkt)
+        #     long_pcaps.append(packet)
+        #     short_stream_count += 1
+        #     short_stream += delta
+        sync_time = -1
+        for time in pkt[ip_tuple][port_tuple]['SYN']:
+            if sync_time == -1:
+                sync_time = time
+            else:
+                if time < sync_time:
+                    sync_time = time
+        for time in pkt[ip_tuple1][port_tuple1]['SYN']:
+            if sync_time == -1:
+                sync_time = time
+            else:
+                if time < sync_time:
+                    sync_time = time
+        if sync_time == -1: continue
+        delta = packet.time-sync_time
+        # print("FIN", ip_tuple, port_tuple, delta)
+        if delta > 10000:
+            long_stream_count += 1
+            long_stream += delta
+            for pkg in pkt[ip_tuple][port_tuple]['PKG']:
+                long_pcaps.append(pkg)
+            for pkg in pkt[ip_tuple1][port_tuple1]['PKG']:
+                short_pcaps.append(pkg)
+            long_pcaps.append(packet)
+        else:
+            short_stream_count += 1
+            short_stream += delta
+            for pkg in pkt[ip_tuple][port_tuple]['PKG']:
+                short_pcaps.append(pkg)
+            for pkg in pkt[ip_tuple1][port_tuple1]['PKG']:
+                short_pcaps.append(pkg)
+            short_pcaps.append(packet)
+        pkt[ip_tuple][port_tuple] = defaultdict(list)
+        pkt[ip_tuple1][port_tuple1] = defaultdict(list)
+        # print(pkt[ip_tuple][port_tuple], pkt[ip_tuple1][port_tuple1])
+        # pkt[ip_tuple][port_tuple].append(("FIN", packet.time))
+    else:
+        if f & SYN:
+            # print("SYN", ip_tuple, port_tuple)
+            pkt[ip_tuple][port_tuple]['SYN'].append(packet.time)
+        # print(ip_tuple, port_tuple)
+        # print("before", pkt[ip_tuple])
+        pkt[ip_tuple][port_tuple]['PKG'].append(packet)
+        # print("after", pkt[ip_tuple])
+
+
+print("Total packages:", len(pcaps))
+print("Total short stream packages:", len(short_pcaps))
+print("Total "+str(long_stream_count)+" long stream sessions")
+print("Total "+str(long_stream)+" long stream length")
+print("Total "+str(short_stream_count)+" short stream sessions")
+print("Total "+str(short_stream)+" short stream length")
+#
+wrpcap("long_stream.pcap", long_pcaps)
+wrpcap("short_stream.pcap", short_pcaps)
+# print(syn, fin)
